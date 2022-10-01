633e4249279a780528fe77cfa80fb94cd6af5100
designsanthu <designsanthu@gmail.com>
2022-10-01 22:02:04 +0530

    first commit

--- src/components/Designs.js
+++ src/components/Designs.js
@@ -1,18 +1,58 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import axios from 'axios'
+import pic2 from '../images/homeCarosel/2.jpg'
+import pic3 from '../images/homeCarosel/3.jpg'
+import pic4 from '../images/homeCarosel/4.jpg'
+import pic5 from '../images/homeCarosel/5.jpg'
+import pic6 from '../images/homeCarosel/6.jpg'
+import pic7 from '../images/homeCarosel/7.jpg'
+import { IoCloseCircleOutline } from 'react-icons/io5'
 
 function Designs() {
 
-    useEffect(() => {
-        axios.get('https://www.instagram.com/design_santhu/channel/?__a=1')
-            .then(res => console.log(res))
-            .catch(err => console.log(err))
-    }, [])
+    const [zoomImg, setImage] = useState("")
+
+    const imgGallery = [
+        {
+            url: pic2
+        },
+        {
+            url: pic3
+        },
+        {
+            url: pic4
+        },
+        {
+            url: pic5
+        },
+        {
+            url: pic6
+        },
+        {
+            url: pic7
+        }
+    ]
+
+
 
     return (
         <div>
 
-        </div>
+            {
+                zoomImg !== "" &&
+                <div className='zoomImgContainer'>
+                    <div className='zoomInImage'>
+                        <span className='imgclosebtn' onClick={() => setImage("")} > <IoCloseCircleOutline /></span>
+                        <img src={zoomImg} className="w-75" />
+                    </div>
+                </div>
+            }
+
+            {
+                imgGallery.map(img =>
+                    <img src={img.url} className="w-50" onClick={() => setImage(img.url)} />)
+            }
+        </div >
     );
 }
 
