6b0cce5f5bd5f918e0b0c868a7da348017f60a4f
designsanthu <designsanthu@gmail.com>
2022-10-03 22:17:51 +0530

    updated design page and home page

--- src/components/Designs.js
+++ src/components/Designs.js
@@ -1,17 +1,47 @@
 import React, { useEffect, useState } from 'react';
-import pic1 from '../images/designs/1.jpg'
-import pic2 from '../images/designs/2.jpg'
-import pic3 from '../images/designs/3.jpg'
-import pic4 from '../images/designs/4.jpg'
-import pic5 from '../images/designs/5.jpg'
-import pic6 from '../images/designs/6.jpg'
 import { IoCloseCircleOutline } from 'react-icons/io5'
+import designs from './designs.json'
 
 function Designs() {
-
+    const [zoomImg, setImage] = useState("")
     return (
         <div>
-            <h5 className='mt-5 text-center'>Designs</h5>
+            <div className="d-none d-sm-block">
+                {
+                    zoomImg !== "" &&
+                    <div className='zoomImgContainer'>
+
+                        <div className='zoomInImage d-flex flex-column'>
+                            <span className='imgclosebtn mb-3' onClick={() => setImage("")} > <IoCloseCircleOutline /></span>
+                            <img src={zoomImg} className="zoom-img rounded" />
+                        </div>
+                    </div>
+                }
+            </div>
+            <div className="d-none d-sm-inline">
+                <div className='row m-0 p-0'>
+                    {
+                        designs.map(img => <div className='card col-md-3 col-sm-6'>
+                            <div className='text-center pt-2 pb-3'>
+                                <img src={img.design_pic} alt='...' onClick={() => setImage(img.design_pic)} className='card-img rounded designs-image' />
+                            </div>
+                        </div>
+                        )
+                    }
+                </div>
+            </div>
+            <div className="d-inline d-sm-none">
+                <div className='row m-0 p-0'>
+                    {
+                        designs.map(img => <div className='card col-md-3 col-sm-6'>
+                            <div className='text-center pt-2 pb-3'>
+                                <img src={img.design_pic} alt='...' onClick={() => setImage("")} className='card-img rounded designs-image' />
+                            </div>
+                        </div>
+                        )
+                    }
+                </div>
+            </div>
         </div >
     );
 }
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,49 +1,85 @@
-import axios from 'axios';
-import React, { useEffect } from 'react';
-import pic1 from '../images/designs/1.jpg'
-import pic2 from '../images/designs/2.jpg'
-import pic3 from '../images/designs/3.jpg'
-import pic4 from '../images/designs/4.jpg'
-import pic5 from '../images/designs/5.jpg'
-import pic6 from '../images/designs/6.jpg'
-// import pic7 from '../images/designs/7.jpeg'
+import React from 'react';
+import albumPic1 from '../images/designs/1.jpg'
+import albumPic2 from '../images/designs/2.jpg'
+import albumPic3 from '../images/designs/3.jpg'
+import albumPic4 from '../images/designs/4.jpg'
+import albumPic5 from '../images/designs/5.jpg'
+import albumPic6 from '../images/designs/6.jpg'
+
 function Home() {
-    useEffect(() => {
-        fetch('https://graph.instagram.com/design_santhu?fields=id,username,media&access_token=IGQVJVQWpiQlZA0bE8tSmU5N25HX3lLbzB4aTl3N2ltMzR6RmE3RVFQX2M2UkRkYnNNcmI3SHJnRzRJLUlhRjltb01XckxkLTRXTVRvNldFaTA1d3d4N29sT2EwVTBOOHdzcUdWLTlNbWc0Y0IyY1pobwZDZD')
-            .then(res => console.log(res))
-    }, [])
+    const designPics = [
+        {
+            designPic1: "https://drive.google.com/uc?export=view&id=1eSaWok5x30RNAYdi1Fr7S7PQgiBcMhq_",
+            designPic2: "https://drive.google.com/uc?export=view&id=1eG3suhmsP8JWcguiPW9h9F9DhatdpawE",
+            designPic3: "https://drive.google.com/uc?export=view&id=1e76g7GwKRwCj3wSOn4v1YzjML7aCdQKP",
+            designPic4: "https://drive.google.com/uc?export=view&id=1dnqGSZDuO0umBlRDSyaLV9rxcKqmXpzn",
+            designPic5: "https://drive.google.com/uc?export=view&id=1ddwsHhqX8u8TLGZm_vFTDyVim9QunXKR",
+            designPic6: "https://drive.google.com/uc?export=view&id=1eNnF2g-PZINTviHvcmGjF_JGLFALhOBL"
+        }
+    ]
     return (
         <div>
-            <div className=''>
+            <div className='d-none d-sm-block'>
                 <div id="carouselExampleControls" className="carousel slide carousel-fade" data-bs-ride="carousel">
                     <div className="carousel-inner">
                         <div className="carousel-item active" data-bs-interval="2000">
-                            <img src={pic1} className="d-block w-100" alt="img" />
+                            <img src={albumPic1} className="d-block w-100" alt="img" />
                         </div>
                         <div className="carousel-item" data-bs-interval="2000">
-                            <img src={pic2} className="d-block w-100" alt="img" />
+                            <img src={albumPic2} className="d-block w-100" alt="img" />
                         </div>
                         <div className="carousel-item" data-bs-interval="2000">
-                            <img src={pic3} className="d-block w-100" alt="img" />
+                            <img src={albumPic3} className="d-block w-100" alt="img" />
                         </div>
                         <div className="carousel-item" data-bs-interval="2000">
-                            <img src={pic4} className="d-block w-100" alt="img" />
+                            <img src={albumPic4} className="d-block w-100" alt="img" />
                         </div>
                         <div className="carousel-item" data-bs-interval="2000">
-                            <img src={pic5} className="d-block w-100" alt="img" />
+                            <img src={albumPic5} className="d-block w-100" alt="img" />
                         </div>
                         <div className="carousel-item" data-bs-interval="2000">
-                            <img src={pic6} className="d-block w-100" alt="img" />
+                            <img src={albumPic6} className="d-block w-100" alt="img" />
                         </div>
-                        {/* <div className="carousel-item" data-bs-interval="2000">
-                            <img src={pic7} className="d-block w-100" alt="img" />
-                        </div> */}
                     </div>
                     <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
                         <span className="carousel-control-prev-icon" aria-hidden="true"></span>
                         <span className="visually-hidden">Previous</span>
                     </button>
                     <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="next">
+                        <span className="carousel-control-next-icon" aria-hidden="true"></span>
+                        <span className="visually-hidden">Next</span>
+                    </button>
+                </div>
+            </div>
+
+            {/* small screens */}
+            <div className='d-block d-sm-none'>
+                <div id="carouselExampleControls1" className="carousel slide carousel-fade" data-bs-ride="carousel">
+                    <div className="carousel-inner">
+                        <div className="carousel-item active" data-bs-interval="2000">
+                            <img src={designPics[0].designPic1} className="d-block w-100" alt="img" />
+                        </div>
+                        <div className="carousel-item" data-bs-interval="2000">
+                            <img src={designPics[0].designPic2} className="d-block w-100" alt="img" />
+                        </div>
+                        <div className="carousel-item" data-bs-interval="2000">
+                            <img src={designPics[0].designPic3} className="d-block w-100" alt="img" />
+                        </div>
+                        <div className="carousel-item" data-bs-interval="2000">
+                            <img src={designPics[0].designPic4} className="d-block w-100" alt="img" />
+                        </div>
+                        <div className="carousel-item" data-bs-interval="2000">
+                            <img src={designPics[0].designPic5} className="d-block w-100" alt="img" />
+                        </div>
+                        <div className="carousel-item" data-bs-interval="2000">
+                            <img src={designPics[0].designPic6} className="d-block w-100" alt="img" />
+                        </div>
+                    </div>
+                    <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls1" data-bs-slide="prev">
+                        <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+                        <span className="visually-hidden">Previous</span>
+                    </button>
+                    <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleControls1" data-bs-slide="next">
                         <span className="carousel-control-next-icon" aria-hidden="true"></span>
                         <span className="visually-hidden">Next</span>
                     </button>
